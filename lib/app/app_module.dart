3d9457daf1dad2ce9007f80c8f585af9347c1815
offKevyn <kevynsilva42@gmail.com>
2024-08-12 19:19:54 -0300

    add: base flutter_modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/home/home_module.dart';
+import 'shared/util/routes_app.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  List<Module> get imports => [];
+
+  @override
+  void routes(r) {
+    r.module(
+      RoutesApp.Home,
+      module: HomeModule(),
+      transition: TransitionType.fadeIn,
+    );
+  }
+}
---
+++ lib/app/modules/home/view/home_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Home',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
