40eae40dc9c9eeb7eb0b197c000bf53e820ecf85
offKevyn <kevynsilva42@gmail.com>
2024-09-15 14:35:44 -0300

    refactor: Update 'FipeDefaultCls' model to handle null values in 'fromMapApi' method

--- lib/app/modules/lookup/model/fipe_default_cls.dart
+++ lib/app/modules/lookup/model/fipe_default_cls.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 
 class FipeDefaultCls {
   final String name;
@@ -11,35 +10,12 @@ class FipeDefaultCls {
 
   static FipeDefaultCls get empty => FipeDefaultCls(name: '', cod: '');
 
-  bool get isEmpty => name.isEmpty || cod.isEmpty;
-
-  bool get isNotEmpty => name.isNotEmpty && cod.isNotEmpty;
-
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'name': name,
-      'cod': cod,
-    };
-  }
-
-  factory FipeDefaultCls.fromMap(Map<String, dynamic> map) {
-    return FipeDefaultCls(
-      name: map['name'] as String,
-      cod: map['cod'] as String,
-    );
-  }
-
   factory FipeDefaultCls.fromMapApi(Map<String, dynamic> map) {
     return FipeDefaultCls(
-      name: map['nome'] as String,
-      cod: map['codigo'].toString(),
+      name: map['nome'] ?? '',
+      cod: map['codigo'] != null ? map['codigo'].toString() : '',
     );
   }
-
-  String toJson() => json.encode(toMap());
-
-  factory FipeDefaultCls.fromJson(String source) =>
-      FipeDefaultCls.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() => name;
