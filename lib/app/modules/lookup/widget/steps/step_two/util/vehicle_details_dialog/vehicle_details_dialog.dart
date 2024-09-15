9e22569f383e1094a7cbac65b854cd17bd3b9389
offKevyn <kevynsilva42@gmail.com>
2024-09-15 20:52:39 -0300

    refactor: add 'stateManagement' in 'vehicle_details_dialog'

--- lib/app/modules/lookup/widget/steps/step_two/util/vehicle_details_dialog/vehicle_details_dialog.dart
+++ lib/app/modules/lookup/widget/steps/step_two/util/vehicle_details_dialog/vehicle_details_dialog.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 import '../../../../../../../shared/util/colors_app.dart';
+import '../../../../../../../shared/util/icon_data_app.dart';
+import '../../../../../../../shared/widget/lottie_custom/lottie_custom.dart';
+import '../../../../../model/vehicle.dart';
 import '../../state/search_vehicle_state.dart';
 
 abstract class VehicleDetailsDialog {
@@ -58,13 +62,150 @@ class _VehicleDetailsDialogContentState
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
       ),
-      content: const SizedBox(
-        height: 120,
-        width: 310,
+      content: SizedBox(
+        height: 300,
+        width: 350,
         child: Center(
-          child: Text('Vehicle Details Dialog'),
+          child: AnimatedBuilder(
+            animation: _searchVehicleState.state,
+            builder: (context, _) {
+              return _stateManagement(
+                state: _searchVehicleState.state.value,
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+
+  Widget _stateManagement({required SearchVehicleTypeState state}) {
+    double? widthLottie = 250;
+
+    switch (state) {
+      case SearchVehicleTypeState.loading:
+        return LottieCustom(
+          width: widthLottie,
+          lottieType: LottieType.loading,
+        );
+      case SearchVehicleTypeState.error:
+        return LottieCustom(
+          width: widthLottie,
+          lottieType: LottieType.failed,
+          onPressed: _refresh,
+        );
+      case SearchVehicleTypeState.empty:
+        return LottieCustom(
+          width: widthLottie,
+          lottieType: LottieType.empty,
+          onPressed: _refresh,
+        );
+      case SearchVehicleTypeState.success:
+        return _success();
+      default:
+        return LottieCustom(
+          width: widthLottie,
+          lottieType: LottieType.loading,
+        );
+    }
+  }
+
+  Widget _success() {
+    Vehicle vehicle = _searchVehicleState.vehicle;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Center(
+          child: PhosphorIcon(
+            IconDataApp.speedometer,
+            color: ColorsApp.secundary,
+            size: 135,
+          ),
+        ),
+        Container(
+          height: 1.2,
+          decoration: BoxDecoration(
+            color: ColorsApp.primary,
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        const SizedBox(height: 10),
+        Expanded(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _fieldInDetails(
+                  title: 'Marca: ',
+                  value: vehicle.brand,
+                ),
+                _fieldInDetails(
+                  title: 'Modelo: ',
+                  value: vehicle.model,
+                ),
+                _fieldInDetails(
+                  title: 'Ano do modelo: ',
+                  value: vehicle.yearOfManufacture.toString(),
+                ),
+                _fieldInDetails(
+                  title: 'Combustível: ',
+                  value: vehicle.fuel,
+                ),
+                _fieldInDetails(
+                  title: 'Valor: ',
+                  value: vehicle.value,
+                ),
+                _fieldInDetails(
+                  title: 'Mês referência: ',
+                  value: vehicle.referenceMonth,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _fieldInDetails({
+    required String title,
+    required String value,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 5,
+        left: 10,
+        right: 10,
+      ),
+      child: Text.rich(
+        TextSpan(
+          style: const TextStyle(
+            fontSize: 16.8,
+            height: 1.08,
+          ),
+          children: [
+            TextSpan(
+              text: title,
+              style: const TextStyle(
+                color: ColorsApp.primary,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            TextSpan(
+              text: value,
+              style: const TextStyle(
+                color: ColorsApp.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _refresh() {
+    _searchVehicleState.refresh();
   }
 }
